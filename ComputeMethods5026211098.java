7087f4499c78f2bd3a6994ec712a0098c1192858
Bayu Liano Leader Habibullah <95364030+bayuliano@users.noreply.github.com>
2022-02-25 11:51:30 +0700

    ComputeMethods5026211098.java
    Converts given temperature in Fahrenheit to Celsius

---
+++ ComputeMethods5026211098.java
@@ -0,0 +1,13 @@
+package ComputeMethods5026211098.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	float Farenheit, Celsius;
+    Farenheit = 100;
+    Celsius = ((Farenheit-32)*5)/9;
+    System.out.println("temperature in celsius is = " + Celsius);
+
+
+    }
+}
